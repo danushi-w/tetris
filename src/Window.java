14e65598f33bd6cee282b1f1d127347f4c62b270
Danushi Weeraman <d_weeraman@hotmail.com>
2022-03-29 23:16:38 +0100

    Added key event listener.

--- src/Window.java
+++ src/Window.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Window {
+import javax.swing.*;
+
+public class Window {
+
+    private JFrame window;
+    private Board board;
+
+    public static final int WIDTH = 500;
+    public static final int HEIGHT = 700;
+
+    public Window() {
+        window = new JFrame("Tetris");
+        window.setSize(WIDTH,HEIGHT);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+
+        board = new Board();
+        window.add(board);
+        window.addKeyListener(board);
+        window.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Window();
+    }
 }
